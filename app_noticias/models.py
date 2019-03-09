2c53247866b33218aecd121e42bd83fe0c2b77fb
Caique Guedes <caique-guedes@hotmail.com>
2019-03-08 21:30:00 -0300

    Teste

---
+++ app_noticias/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+class Noticia(models.Model):
+    class Meta:
+        verbose_name = "Notícia"
+        verbose_name_plural = "Notícias"
+
+    titulo = models.CharField("título", max_length=128)
+    conteudo = models.TextField()
+    data_criacao = models.DateTimeField()
+    data_publicacao = models.DateTimeField()
+    publicado = models.BooleanField()
+    
